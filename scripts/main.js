33489fd3cc88ecf1a9e30274f853ba2dd6a214d9
miguel <91511269+ghmacg@users.noreply.github.com>
2023-08-21 15:15:00 -0400

    Create function to get winner of round

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
 const choicesArray = ['rock', 'paper', 'scissors']
 
 const getComputerChoice = (array) => array[(Math.floor(Math.random() * array.length))]
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log('tie');
+    } else if (
+        ((playerSelection == choicesArray[0]) && (computerSelection == choicesArray[2])) || 
+        ((playerSelection == choicesArray[1]) && (computerSelection == choicesArray[0])) ||
+        ((playerSelection == choicesArray[2]) && (computerSelection == choicesArray[1]))
+    ) {
+        console.log('player wins')
+    } else {
+        console.log('computer wins')
+    }
+}
